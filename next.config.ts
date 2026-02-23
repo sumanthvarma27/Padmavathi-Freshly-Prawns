f86690f66d96f53d6661571dbb493a01191f3941
sumanthvarma2001 <sumanthvarma727@gmail.com>
2026-02-23 18:03:34 -0500

    initial commit: config files

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: "/Users/sumanthvarma/Desktop/padmavathi-admin"
+  }
+};
+
+export default nextConfig;
